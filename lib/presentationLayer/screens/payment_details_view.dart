a1de01cad65070f825dfa129b9a512c9b13934f8
ahmed omar <ahmedomar1997.aoo@gmail.com>
2025-04-14 17:33:12 +0200

    update

--- lib/presentationLayer/screens/payment_details_view.dart
+++ lib/presentationLayer/screens/payment_details_view.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
-import '../../core/utils/style.dart';
 import '../../dataLayer/cubit/app_cubit.dart';
 import '../../dataLayer/cubit/app_state.dart';
 import '../helper/snakbar_error.dart';
-import '../widget/cart_info.dart';
 import '../widget/custom_appBar.dart';
-import '../widget/mainButton.dart';
 import '../widget/payment_item.dart';
-import '../widget/total_price.dart';
 
 class PaymentDetails extends StatelessWidget {
   const PaymentDetails({super.key});
@@ -28,44 +23,7 @@ class PaymentDetails extends StatelessWidget {
           body: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(height: 25),
-                  Payment(),
-                  // SizedBox(height: 25),
-                  // OrderInfoItem(value: '\$42.97', title: 'Order Subtotal'),
-                  // SizedBox(height: 3),
-                  // OrderInfoItem(value: '\$0', title: 'Discount'),
-                  // SizedBox(height: 3),
-                  // OrderInfoItem(value: '\$8', title: 'Shipping'),
-                  // SizedBox(height: 20),
-                  // //  Divider(
-                  // //   color: const Color(0xFFC6C6C6),
-                  // //   thickness: 2,),
-                  // Container(
-                  //   width: 320,
-                  //   decoration: ShapeDecoration(
-                  //     shape: RoundedRectangleBorder(
-                  //       side: BorderSide(
-                  //         width: 2,
-                  //         strokeAlign: BorderSide.strokeAlignCenter,
-                  //         color: const Color(0xFFC6C6C6),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                  // SizedBox(height: 20),
-                  // TotalPrice(value: '\$50.97', title: 'Total'),
-                  // SizedBox(height: 50),
-                  // MainButton(
-                  //   width: 350,
-                  //   hight: 73,
-                  //   label: 'Complete Payment',
-                  //   function: () {},
-                  //   color: const Color(0xFF34A853),
-                  // ),
-                ],
-              ),
+              child: Column(children: [SizedBox(height: 25), Payment()]),
             ),
           ),
         );
@@ -76,7 +34,7 @@ class PaymentDetails extends StatelessWidget {
 
 // ممكن دي اعنلها كا model
 class Payment extends StatefulWidget {
-  Payment({super.key});
+  const Payment({super.key});
 
   @override
   State<Payment> createState() => _PaymentState();
